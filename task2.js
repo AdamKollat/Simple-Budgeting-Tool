a9cc3a679529a5b2cb02c43f30cf4030f30b9ad0
Adam <adam.kollat@gmail.com>
2022-12-18 12:31:00 +0000

    Added 5 expense objects

--- task2.js
+++ task2.js
@@ -2,7 +2,7 @@ let income = [];
 let expenses = [];
 
 function initialise() {
-    if (sessionStorage.getItem(hasCodeRunBefore) === null) {
+    if (sessionStorage.getItem("hasCodeRunBefore") === null) {
         sessionStorage.setItem("income", JSON.stringify(income));
         sessionStorage.setItem("expenses", JSON.stringify(expenses));
         sessionStorage.setItem("hasCodeRunBefore", true);
@@ -29,3 +29,9 @@ function Expense(name, amount, recurring) {
     this.amount = amount;
     this.recurring = recurring;
 }
+
+let expense1 = expenses.push(new Expense("Rent", 500, true));
+let expense2 = expenses.push(new Expense("Groceries", 300, true));
+let expense3 = expenses.push(new Expense("Fuel", 100, true));
+let expense4 = expenses.push(new Expense("Cinema", 30, false));
+let expense5 = expenses.push(new Expense("Bowling", 25, false));
